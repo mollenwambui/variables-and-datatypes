06d4b518c8308d738aea7570c9177afc190f1dc4
mollenwambui <mollenwambui@gmail.com>
2022-02-12 07:33:33 +0300

    first commit

---
+++ src/main/kotlin/Main.kt
@@ -0,0 +1,15 @@
+fun main (){
+    var name ="Mollen wambui"
+    println (name)
+    var age = "21"
+    println (age)
+    var phonenumber = "0714573892"
+    println (phonenumber)
+     var weight ="49"
+     println (weight)
+    val iskenyan: Boolean= true
+    val isnotkenyan: Boolean= false
+    println(iskenyan)  //output true
+    println(isnotkenyan) //output false
+
+}
